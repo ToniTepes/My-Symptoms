ca8fe3b7961ec5e21d05303fd911defb60ac5a63
robert <robert.tarkenton@gmail.com>
2019-02-19 15:21:59 -0500

    initial model creation

---
+++ models/users.js
@@ -0,0 +1,34 @@
+module.exports = function(sequelize, DataTypes) {
+
+    var user = sequelize.define("user", {
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1, 50]
+            }
+        },
+
+        firstname: {
+            type: DataTypes.TEXT,
+            allowNull: false
+        },
+
+        lastname: {
+            type: DataTypes.TEXT,
+            allowNull: false
+        },
+
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                min: 6
+            }
+        },
+        createdAt: Sequelize.DATE,
+        updatedAt: Sequelize.DATE
+
+    });
+    return user;
+};
