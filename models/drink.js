3a5823b125523f83eba32e0ea0ec1b69286fc67e
robert <robert.tarkenton@gmail.com>
2019-02-20 18:24:37 -0500

    formatting

--- models/drink.js
+++ models/drink.js
@@ -1,12 +1,11 @@
 module.exports = function(sequelize, DataTypes) {
-
-    var drink = sequelize.define("drink", {
-        name: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        createdAt: Sequelize.DATE,
-        updatedAt: Sequelize.DATE
-    });
-    return drink;
+  var drink = sequelize.define("drink", {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    createdAt: Sequelize.DATE,
+    updatedAt: Sequelize.DATE
+  });
+  return drink;
 };
