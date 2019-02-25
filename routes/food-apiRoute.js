5a9e333efd63a5639a7c30094b4eefb66979a7c7
robert <robert.tarkenton@gmail.com>
2019-02-25 17:50:00 -0500

    removed unused legacy files

--- routes/food-apiRoute.js
+++
@@ -1,6 +0,0 @@
-const foodController = require("../controllers/foodController");
-
-module.exports = function(app) {
-  // Posts a food if user is logged in, otherwise redirect to signin page
-  app.post("/api/food", foodController.postFood);
-};
