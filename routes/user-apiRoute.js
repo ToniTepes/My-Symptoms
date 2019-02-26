ece75438b7a907c910841eb9236976ffae108ecd
robert <robert.tarkenton@gmail.com>
2019-02-26 12:25:31 -0500

    updated routes dir

--- routes/user-apiRoute.js
+++
@@ -1,9 +0,0 @@
-const userController = require("../controllers/userController");
-
-module.exports = function(app) {
-  app.get("/api/users", userController.getUsers);
-
-  app.get("/api/users/:id", userController.getUserById);
-
-  app.delete("/api/users/:id", userController.deleteUser);
-};
