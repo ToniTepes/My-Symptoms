384621666690ca6a7311603af5a4e5b36786ec84
robert <robert.tarkenton@gmail.com>
2019-02-26 12:53:28 -0500

    updated html to src current js, added moment to server.js

--- schema.sql
+++ schema.sql
@@ -3,28 +3,31 @@
 
 USE `deardoctor_db`;
 
-CREATE TABLE IF NOT EXISTS `diary` 
+  CREATE TABLE `users`
+  	(
+  		user_id INTEGER AUTO_INCREMENT UNIQUE,
+  		email VARCHAR(150) UNIQUE,
+  		firstName VARCHAR(50) ,
+ 		lastName VARCHAR(50) ,
+ 		password VARCHAR(255) ,
+ 		createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+ 		updatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+--  		-- heal_id INTEGER,
+  		PRIMARY KEY(user_id)
+--  		-- FOREIGN KEY(heal_id) REFERENCES patientInfo(heal_id)
+  	);
+
+CREATE TABLE `diary` 
 (
-	`id` INTEGER NOT NULL auto_increment , 
+	`id` INTEGER NOT NULL PRIMARY KEY, 
     `symptom` JSON DEFAULT NULL, 
     `food` JSON DEFAULT NULL, 
     `drink` JSON DEFAULT NULL, 
     `createdAt` DATETIME NOT NULL, 
     `updatedAt` DATETIME NOT NULL, 
-    PRIMARY KEY (`id`)
+    `owner_id` INT,
+    INDEX (owner_id),
+    FOREIGN KEY (owner_id) REFERENCES users(user_id)
     ) 
     ENGINE=InnoDB;
-
-    --  CREATE TABLE `users`
---  	(
---  		user_id INTEGER AUTO_INCREMENT UNIQUE,
---  		email VARCHAR(150) UNIQUE,
---  		firstname VARCHAR(50) ,
---  		lastname VARCHAR(50) ,
---  		pass VARCHAR(255) ,
--- 		createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
--- 		updatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
---  		-- heal_id INTEGER,
---  		PRIMARY KEY(user_id)
---  		-- FOREIGN KEY(heal_id) REFERENCES patientInfo(heal_id)
---  	);
+	
