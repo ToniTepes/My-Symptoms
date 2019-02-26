e665e4db3810ba3b14cbd8cbb03811b0899695c0
Sydney Dunlap <sydney.dunlap2011@gmail.com>
2019-02-25 22:43:56 -0500

    updated labeling on signupjs file to match sign up file

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,22 +1,28 @@
 $(document).ready(function() {
   // Getting references to our form and input
-  var signUpForm = $("form.signup");
-  var emailInput = $("input#email-input");
-  var passwordInput = $("input#password-input");
+  var signUpForm = $("signupcontainer");
+  var firstName = $("#firstname");
+  var lastName = $("#lastname");
+  var emailInput = $("#email");
+  var passwordInput = $("#password");
 
   // When the signup button is clicked, we validate the email and password are not blank
   signUpForm.on("submit", function(event) {
     event.preventDefault();
     var userData = {
+      firstName: firstName.val().trim(),
+      lastName: lastName.val().trim(),
       email: emailInput.val().trim(),
       password: passwordInput.val().trim()
     };
 
-    if (!userData.email || !userData.password) {
+    if (!userData.email || !userData.password || !userData.firstName || !userData.lastName) {
       return;
     }
     // If we have an email and password, run the signUpUser function
-    signUpUser(userData.email, userData.password);
+    signUpUser(userData.firstName, userData.lastName, userData.email, userData.password);
+    firstName.val("");
+    lastName.val("");
     emailInput.val("");
     passwordInput.val("");
   });
@@ -25,6 +31,8 @@ $(document).ready(function() {
   // Otherwise we log any errors
   function signUpUser(email, password) {
     $.post("/api/signup", {
+      firstName: firstName,
+      lastName: lastName,
       email: email,
       password: password
     }).then(function(data) {
